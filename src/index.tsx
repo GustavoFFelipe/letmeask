2bf0fdf5cb8fb5e98c6d3924ba7f1922b28935b5
GustavoFFelipe <gustavo.ffelipe19@gmail.com>
2021-12-19 20:54:28 -0300

    finalizacao da home

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// firebase services
 import 'firebase/database';
 import './services/firebase';
+
+//css
+import './styles/global.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
